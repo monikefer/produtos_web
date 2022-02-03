c3d5df5afdcff7980c0421b75091a9cb7e47d5b6
monikefer <mvfer2014@gmail.com>
2022-02-03 16:54:00 -0300

    first commit

---
+++ src/components/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import LoginPage from '../pages/LoginPage';
+import RegisterPage from '../pages/RegisterPage';
+import PasswordPage from '../pages/PasswordPage';
+//declarando o componente utilizando sintaxe de função
+const Main = () => (
+    <div className="container mt-4">
+        <Routes>
+         <Route path="/" exact element={<LoginPage />} />
+         <Route path="/cadastrar-usuário" element={<RegisterPage />} />
+         <Route path="/esqueci-minha-senha" element={<PasswordPage />} />
+        </Routes>
+    </div>
+)
+export default Main;
